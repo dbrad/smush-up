fd6c2bd032c160eb0abafd12450ab7924c0f3b82
David Brad <davidbrad.015@gmail.com>
2015-07-07 17:01:13 -0400

    More changes to JSON data.

--- public/js/app.js
+++ public/js/app.js
@@ -2,10 +2,50 @@
   angular.module("smushUp", []);
 
   angular.module('smushUp').factory('SmashUp', function($http, $q) {
-    var deffered = $q.defer();
+    var SmashUp = {};
+
+    SmashUp.obv = [];
+    SmashUp.data = {};
+    SmashUp.regObv = function(callback) {
+      SmashUp.obv.push(callback);
+    };
+    SmashUp.notify = function() {
+      for( var o in SmashUp.obv ) {
+        SmashUp.obv[o]();
+      }
+    };
+
     $http.get('/factions.json').success( function (data) {
-      deffered.resolve(data);
+      SmashUp.data = data;
+      SmashUp.notify();
     });
-    return deffered.promise;
+
+    return SmashUp;
+  });
+
+  angular.module('smushUp').controller("DeckPickerController", function($rootScope, SmashUp) {
+    var self = this;
+    this.sets = {};
+
+    this.updateSets = function() {
+      self.sets = SmashUp.data;
+    };
+    SmashUp.regObv(this.updateSets);
+    this.getStage = function() {
+      return $rootScope.Stage;
+    };
+    this.setStage = function( value ) {
+      $rootScope.Stage = value;
+    };
+
+    this.getSelectedCount = function() {
+      return _.countBy(self.sets, function() {
+        return (self.sets.selected ? "Sel" : "NotSel");
+      });
+    };
+
+    this.deckPickStage = function() {
+      return self.getStage() === 1;
+    };
   });
 })();
--- public/js/set_controller.js
+++ public/js/set_controller.js
@@ -1,70 +1,82 @@
 (function() {
-  angular.module("smushUp").controller("SetController", SetController);
+  angular.module("smushUp").controller("SetController", ["$rootScope", "SmashUp", SetController]);
 
-  function SetController(SmashUp) {
-      var self = this;
-      this.sets = {};
-      this.showDecks = false;
-      var lockedIn = false;
+  function SetController($rootScope, SmashUp) {
+    if(!$rootScope.Stage)
+      $rootScope.Stage = 0;
 
-      SmashUp.then(function(data) {
-        self.sets = data;
-      });
+    var self = this;
+    this.sets = {};
+    this.showDecks = false;
 
-      this.parentCheck = function( _set ) {
+    this.updateSets = function() {
+      self.sets = SmashUp.data;
+    };
+    SmashUp.regObv(this.updateSets);
+    this.getStage = function() {
+      return $rootScope.Stage;
+    };
+    this.setStage = function( value ) {
+      $rootScope.Stage = value;
+    };
+
+    this.parentCheck = function( _set ) {
+      for( var f in _set.factions ) {
+        var faction = _set.factions[f];
+        faction.selected = _set.selected;
+      }
+    };
+
+    this.childCheck = function( _set, _faction ) {
+      if( _faction.selected )
+        _set.selected = true;
+      else {
+        var anyCheck = false;
         for( var f in _set.factions ) {
           var faction = _set.factions[f];
-          faction.selected = _set.selected;
+          anyCheck = anyCheck || faction.selected;
         }
-      };
+        if(!anyCheck)
+          _set.selected = false;
+      }
+    };
 
-      this.childCheck = function( _set, _faction ) {
-        if( _faction.selected )
-          _set.selected = true;
-        else {
-          var anyCheck = false;
-          for( var f in _set.factions ) {
-            var faction = _set.factions[f];
-            anyCheck = anyCheck || faction.selected;
-          }
-          if(!anyCheck)
-            _set.selected = false;
+    this.checkAll = function() {
+      for( var s in self.sets ) {
+        var set = self.sets[s];
+        set.selected = true;
+        for( var f in set.factions ) {
+          var faction = set.factions[f];
+          faction.selected = true;
         }
-      };
+      }
+    };
 
-      this.checkAll = function() {
-        for( var s in self.sets ) {
-          var set = self.sets[s];
-          set.selected = true;
-          for( var f in set.factions ) {
-            var faction = set.factions[f];
-            faction.selected = true;
-          }
+    this.uncheckAll = function() {
+      for( var s in self.sets ) {
+        var set = self.sets[s];
+        set.selected = false;
+        for( var f in set.factions ) {
+          var faction = set.factions[f];
+          faction.selected = false;
         }
-      };
+      }
+    };
 
-      this.uncheckAll = function() {
-        for( var s in self.sets ) {
-          var set = self.sets[s];
-          set.selected = false;
-          for( var f in set.factions ) {
-            var faction = set.factions[f];
-            faction.selected = false;
-          }
-        }
-      };
+    this.toggleShowDecks = function() {
+      self.showDecks = !self.showDecks;
+    };
 
-      this.toggleShowDecks = function() {
-        self.showDecks = !self.showDecks;
-      };
+    this.setPickStage = function() {
+      return (self.getStage() === 0);
+    };
 
-      this.lockedIn = function() {
-        return lockedIn;
-      };
-
-      this.lockIn = function() {
-        lockedIn = true;
-      };
-
-}
+    this.moveToDeck = function() {
+      self.setStage(1);
+      SmashUp.data = self.sets;
+      console.log(self.sets);
+      console.log(SmashUp.data);
+      SmashUp.notify();
+    };
+  }
 })();
